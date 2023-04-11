aa28d88ab1cf8946a05a38bf4e8710840c48692e
Daniil Marakhovskiy <danya4art@gmail.com>
2023-04-11 17:41:21 +0300

    Fix converter

--- converter.py
+++ converter.py
@@ -6,34 +6,29 @@ pattern = r'\$\{(?P<var>\w+)\}'
 
 class Converter:
 
-    # def __init__(self, config_path=None):
-    #     urllib3.disable_warnings()
-    #     if config_path is None:
-    #         config_path = '~/.kube/config'
-    #     self.client = openshift.dynamic.DynamicClient(kubernetes.config.new_client_from_config(config_path))
+    def convert_template(self, source, target):
+        with open(source, 'r') as input_file:
+            template = yaml.safe_load(input_file)
+        params = template.pop('parameters')
+        new_params = list(map(lambda d: {d['name'].lower(): d['value']}, params))
+        with open(f'{target}/values.yaml', 'w') as output_file:
+            yaml.safe_dump(new_params, output_file, default_flow_style=False, explicit_start=True, allow_unicode=True)
+        converted = self._convert(template['objects'])
+        kinds = set(filter(lambda o: o['kind'], converted))
+        for kind in kinds:
+            with open(f'{target}/templates/{kind}.yaml', 'w') as output_file:
+                objects = filter(lambda obj: obj['kind'] == kind, converted)
+                yaml.safe_dump(objects, output_file, default_flow_style=False, explicit_start=True, allow_unicode=True)
 
-    def convert(self, source, target):
-        man = None
-        with open(source, 'r') as input:
-            man = yaml.safe_load(input)
-        converted = self._convert(man)
-        with open(f'{target}/values', 'w') as output:
-            output
-
-
-    def _convert(self, manifest: dict):
-        changes = set()
-        manifest = dict()
-        for v in manifest.values():
+    def _convert(self, manifest: dict = None):
+        if manifest is None:
+            manifest = {}
+        for k, v in manifest.values():
             if isinstance(v, dict):
-                res = self._convert(v)
-                changes = changes.union(res['changes'])
-                manifest = res['manifest']
-            elif match := re.findall(pattern, v):
-                v = re.sub(pattern, self.change_var_template, v)
-
-
-        return {'changes': changes, 'manifest': manifest}
+                manifest[k] = self._convert(v)
+            elif re.search(pattern, v):
+                manifest[k] = re.sub(pattern, self.change_var_template, v)
+        return manifest
 
     @staticmethod
     def change_var_template(match_obj):
